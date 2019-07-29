cb7e2c2834ac561ee23974aeb3d7bedb48719ad3
Nidhinesh <nndihinesh@gmail.com>
2019-07-29 19:38:39 +1200

    Initial code provided

---
+++ cmake-build-debug/CMakeFiles/370a1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/370a1.dir/main.cpp.o"
+  "370a1.pdb"
+  "370a1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/370a1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
